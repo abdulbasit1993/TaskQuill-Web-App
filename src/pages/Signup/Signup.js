1cfb30680eace09411c454245aaf06c3464f6996
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2023-06-04 00:44:59 +0500

    working on signup API integration

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -17,21 +17,97 @@ import InputAdornment from "@mui/material/InputAdornment";
 import IconButton from "@mui/material/IconButton";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { authService } from "../../services/authService";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Loader from "../../components/Loader/Loader";
 
 const defaultTheme = createTheme();
 
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
 const Signup = () => {
+  const [data, setData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    firstName: "",
+    lastName: "",
+    phone: "",
+    address: "",
+    occupation: "",
+    aboutMe: "",
+  });
+
   const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    let username = data?.username.trim();
+    let email = data?.email.trim();
+    let password = data?.password.trim();
+    let confirmPassword = data?.confirmPassword.trim();
+
+    const dataObj = {
+      username: username,
+      email: email,
+      password: password,
+      firstName: data?.firstName,
+      lastName: data?.lastName,
+      phone: data?.phone,
+      address: data?.address,
+      occupation: data?.occupation,
+      aboutMe: data?.aboutMe,
+    };
+
+    if (!username) {
+      toast("Please enter your username", { type: "error" });
+      setIsLoading(false);
+      return;
+    }
+
+    if (!email) {
+      toast("Please enter your email", { type: "error" });
+      setIsLoading(false);
+      return;
+    }
+
+    if (!email.match(emailRegex)) {
+      toast("Please enter a valid email", { type: "error" });
+      setIsLoading(false);
+      return;
+    }
+
+    if (!password) {
+      toast("Please enter your password", { type: "error" });
+      setIsLoading(false);
+      return;
+    }
+
+    if (password != confirmPassword) {
+      toast("Passwords Do Not Match!", { type: "error" });
+      setIsLoading(false);
+      return;
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("submit form...");
+  const handleClickShowConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword);
   };
 
   useEffect(() => {
@@ -65,15 +141,32 @@ const Signup = () => {
             Create a New Account
           </Typography>
 
-          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            {/* ================= Username Input Field ================= */}
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              name="username"
+              label="Username"
+              onChange={handleInputChange}
+            />
+
             {/* ================= Email Input Field ================= */}
             <TextField
               margin="normal"
               required
               fullWidth
               id="email"
-              label="Email Address"
               name="email"
+              label="Email Address"
+              onChange={handleInputChange}
               autoComplete="email"
               autoFocus
             />
@@ -83,10 +176,11 @@ const Signup = () => {
               margin="normal"
               required
               fullWidth
+              id="password"
               name="password"
               label="Password"
+              onChange={handleInputChange}
               type={showPassword ? "text" : "password"}
-              id="password"
               autoComplete="current-password"
               InputProps={{
                 endAdornment: (
@@ -103,21 +197,100 @@ const Signup = () => {
               }}
             />
 
-            {/* ================= Remember me checkbox ================= */}
+            {/* ================= Confirm Password Input Field ================= */}
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="confirm-password"
+              name="confirmPassword"
+              label="Confirm Password"
+              onChange={handleInputChange}
+              type={showConfirmPassword ? "text" : "password"}
+              autoComplete="current-password"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => handleClickShowConfirmPassword()}
+                      edge="end"
+                    >
+                      {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
 
-            {/* <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            /> */}
+            {/* ================= First Name Input Field ================= */}
+            <TextField
+              margin="normal"
+              fullWidth
+              id="firstName"
+              name="firstName"
+              label="First Name"
+              onChange={handleInputChange}
+            />
 
-            {/* ================= Login button ================= */}
+            {/* ================= Last Name Input Field ================= */}
+            <TextField
+              margin="normal"
+              fullWidth
+              id="lastName"
+              name="lastName"
+              label="Last Name"
+              onChange={handleInputChange}
+            />
+
+            {/* ================= Phone Input Field ================= */}
+            <TextField
+              margin="normal"
+              fullWidth
+              id="phone"
+              name="phone"
+              label="Phone number"
+              onChange={handleInputChange}
+            />
+
+            {/* ================= Address Input Field ================= */}
+            <TextField
+              margin="normal"
+              fullWidth
+              id="address"
+              name="address"
+              label="Address"
+              onChange={handleInputChange}
+            />
+
+            {/* ================= Occupation Input Field ================= */}
+            <TextField
+              margin="normal"
+              fullWidth
+              id="occupation"
+              name="occupation"
+              label="Occupation"
+              onChange={handleInputChange}
+            />
+
+            {/* ================= About Me Input Field ================= */}
+            <TextField
+              margin="normal"
+              fullWidth
+              id="aboutMe"
+              name="aboutMe"
+              label="About Me"
+              onChange={handleInputChange}
+            />
+
+            {/* ================= Signup button ================= */}
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Signup
+              Sign up
             </Button>
 
             <Grid container justifyContent="center" alignItems="center">
