e60a95f23c50943becbefc286dae46ccc95766cc
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2023-06-04 23:59:16 +0500

    added a new API URL

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -1,7 +1,9 @@
 const currentEnv = process.env.NODE_ENV;
 
-const DEV_API_URL = "https://light-eel-sundress.cyclic.app/api";
-const PROD_API_URL = "https://light-eel-sundress.cyclic.app/api";
+// const API_URL_OLD = "https://light-eel-sundress.cyclic.app/api";
+
+const DEV_API_URL = "https://picayune-twisty-camera.glitch.me/api";
+const PROD_API_URL = "https://picayune-twisty-camera.glitch.me/api";
 
 export const BASE_URL =
   currentEnv === "development" ? DEV_API_URL : PROD_API_URL;
